4ffbf0a3d3f5d765ef5c161dafe8e558121f3f99
aldaniluk <allldaniluk@gmail.com>
2017-07-27 10:27:32 +0300

    Added 'Fibonacci' class

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Logic;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Fibonacci
+            IEnumerable<int> numerator = Fibonacci.Generate(8);
+            foreach(int i in numerator)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+
+
+        }
+    }
+}
---
+++ Logic/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Generates sequence of Fibonacci numbers.
+        /// </summary>
+        /// <param name="quantity">Quantity of numbers to generate.</param>
+        /// <returns>Enumerator to iterate.</returns>
+        public static IEnumerable<int> Generate(int quantity)
+        {
+            if (quantity <= 0) throw new ArgumentException($"{nameof(quantity)} is unsuitable.");
+
+            int prevNumber = -1;
+            int nextNumber = 1;
+            int temp;
+            for(int i = 0; i < quantity; i++)
+            {
+                temp = nextNumber;
+                yield return nextNumber += prevNumber;
+                prevNumber = temp;
+            }
+        }
+    }
+}
